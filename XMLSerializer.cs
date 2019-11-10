1b4f145ab41f686076eabb2d42e563d07e8db04f
MrJackSpade <loffredo.ap@gmail.com>
2019-11-10 00:51:37 -0700

    Auto Commit

--- XMLSerializer.cs
+++ XMLSerializer.cs
@@ -18,7 +18,7 @@ namespace Penguin.Reflection.Serialization.XML
     public class XMLSerializer
     {
         Dictionary<Type, Dictionary<string, PropertyInfo>> PropertyCache { get; set; } = new Dictionary<Type, Dictionary<string, PropertyInfo>>();
-        
+
         /// <summary>
         /// Defines the PropertyInfo for a given type used to map during deserialization. Should allow the Deserializer to skip properties by 
         /// specifying only the properties you want deserialized
@@ -42,7 +42,8 @@ namespace Penguin.Reflection.Serialization.XML
             if (Options.CaseSensitive)
             {
                 props = new Dictionary<string, PropertyInfo>();
-            } else
+            }
+            else
             {
                 props = new Dictionary<string, PropertyInfo>(StringComparer.OrdinalIgnoreCase);
             }
@@ -114,14 +115,14 @@ namespace Penguin.Reflection.Serialization.XML
             return sb.ToString();
         }
 
-        internal  (string propName, char lastChar) GetNextPropertyName(TextReader reader)
+        internal (string propName, char lastChar) GetNextPropertyName(TextReader reader)
         {
             StringBuilder s = new StringBuilder(20);
 
             char c = (char)reader.Read();
 
-            
-            while(!char.IsWhiteSpace(c) && c != '>')
+
+            while (!char.IsWhiteSpace(c) && c != '>')
             {
                 if (c == '-')
                 {
@@ -147,13 +148,14 @@ namespace Penguin.Reflection.Serialization.XML
         /// <returns>The deserialized object</returns>
         public T DeserializeObject<T>(TextReader reader) where T : class
         {
-            if((reader as System.IO.StreamReader).BaseStream.Position == (reader as System.IO.StreamReader).BaseStream.Length){
-                return null;
+            if (reader is StreamReader sReader)
+            {
+                if (sReader.BaseStream.Position == sReader.BaseStream.Length)
+                {
+                    return null;
+                }
             }
 
-            T o = Activator.CreateInstance<T>();
-
-            Dictionary<string, PropertyInfo> props = GetProperties(o.GetType());
 
             if (Options.StartNode != null)
             {
@@ -163,44 +165,7 @@ namespace Penguin.Reflection.Serialization.XML
             reader.AdvancePast(">");
 
 
-            int i = reader.Read();
-
-            while (i != -1)
-            {
-
-                char c = (char)i;
-
-                while (c != '<')
-                {
-                    c = (char)reader.Read();
-                }
-
-                (string propName, char lastChar) = GetNextPropertyName(reader);
-
-                if (propName[0] == '/')
-                {
-                    return o;
-                }
-
-
-                if (lastChar != '>')
-                {
-                    reader.AdvancePast('>');
-                }
-
-
-                if (propName[propName.Length - 1] == '/') {
-                    //self closing, skip
-                } else if (props.TryGetValue(propName, out PropertyInfo pInfo))
-                {
-                    pInfo.SetValue(o, GetValue(pInfo.PropertyType, reader));
-                } else
-                {
-                    reader.AdvancePast($"/{propName}>");
-                }
-            }
-
-            return null;
+            return GetValue(typeof(T), reader, '>') as T;
         }
         /// <summary>
         /// Deserializes an XML string to the given object type
@@ -219,165 +184,173 @@ namespace Penguin.Reflection.Serialization.XML
         {
             char c = lastChar;
 
-            while (char.IsWhiteSpace(c))
+
+            if (pType == typeof(string) || pType.IsValueType)
             {
                 c = (char)reader.Read();
-            }
 
-            if (c == '<')
-            {
-                if(pType == typeof(string))
+                StringBuilder toReturn = new StringBuilder(25);
+
+                while (c != '<')
                 {
-                    reader.AdvancePast('>');
-
-                    c = (char)reader.Read();
-
-                    StringBuilder toReturn = new StringBuilder(25);
-
-                    while(c != '<')
-                    {
-                        toReturn.Append(c);
-                        c = (char)reader.Read();
-                    }
-
-                    reader.AdvancePast('>');
-
-                    return toReturn.ToString();
-
-                }
-
-                object o = Activator.CreateInstance(pType);
-
-                if (o is IList)
-                {
-                    Type collectionType = pType.GetCollectionType();
-
-                    do
-                    {
-                        (o as IList).Add(GetValue(collectionType, reader, c));
-
-                        do
-                        {
-                            c = (char)reader.Read();
-                        } while (char.IsWhiteSpace(c));
-
-                    } while ((char)reader.Peek() != '/');
-
-                    reader.AdvancePast('>');
-                }
-                else
-                {
-
-
-                    Dictionary<string, PropertyInfo> props = GetProperties(pType);
-
-                    string propName;
-
-                    (propName, lastChar) = GetNextPropertyName(reader);
-
-                    if (Options.AttributesAsProperties)
-                    {
-                        
-                        while (lastChar != '>')
-                        {
-                            StringBuilder prop = new StringBuilder(20);
-
-                            StringBuilder val = new StringBuilder(20);
-
-                            c = lastChar;
-
-                            while (char.IsWhiteSpace(c))
-                            {
-                                c = (char)reader.Read();
-                            }
-
-                            do
-                            {
-                                prop.Append(c);
-                                c = (char)reader.Read();
-                            } while (c != '=');
-
-                            reader.AdvancePast('"');
-
-                            c = (char)reader.Read();
-
-                            string pName = prop.ToString();
-
-
-                            if (props.TryGetValue(pName, out PropertyInfo pInfo))
-                            {
-                                do
-                                {
-                                    val.Append(c);
-                                    c = (char)reader.Read();
-                                } while (c != '"');
-
-                                pInfo.SetValue(o, val.ToString().Convert(pInfo.PropertyType));
-                            }
-                            else
-                            {
-                                do
-                                {
-                                    c = (char)reader.Read();
-                                } while (c != '"');
-                            }
-
-                            do
-                            {
-                                c = (char)reader.Read();
-                            } while (char.IsWhiteSpace(c));
-
-                            if(c == '/')
-                            {
-                                reader.AdvancePast('>');
-                                return o;
-                            }
-
-                            lastChar = c;
-                        }
-                    }
-
-                    if (lastChar != '>')
-                    {
-                        reader.AdvancePast('>');
-                    }
-
-                    do
-                    {
-
-                        if (props.TryGetValue(propName, out PropertyInfo pInfo))
-                        {
-                            pInfo.SetValue(o, GetValue(pInfo.PropertyType, reader));
-                        }
-
-                        do
-                        {
-                            c = (char)reader.Read();
-                        } while (char.IsWhiteSpace(c));
-
-                        (propName, _) = GetNextPropertyName(reader);
-
-
-                    } while (propName[0] != '/');
-
-                }
-
-                return o;
-
-            } else
-            {
-                StringBuilder propValue = new StringBuilder(20);
-
-                while(c != '<')
-                {
-                    propValue.Append(c);
+                    toReturn.Append(c);
                     c = (char)reader.Read();
                 }
 
                 reader.AdvancePast('>');
 
-                return propValue.ToString().Convert(pType); 
+                return toReturn.ToString().Convert(pType);
+
             }
+
+            while (char.IsWhiteSpace(c))
+            {
+                c = (char)reader.Read();
+            }
+
+            object o = Activator.CreateInstance(pType);
+
+            if (o is IList)
+            {
+                Type collectionType = pType.GetCollectionType();
+
+                do
+                {
+                    (o as IList).Add(GetValue(collectionType, reader, c));
+
+                    do
+                    {
+                        c = (char)reader.Read();
+                    } while (char.IsWhiteSpace(c));
+
+                } while ((char)reader.Peek() != '/');
+
+                reader.AdvancePast('>');
+            }
+            else
+            {
+                reader.AdvancePast("<");
+
+                Dictionary<string, PropertyInfo> props = GetProperties(pType);
+
+                string propName;
+
+                (propName, lastChar) = GetNextPropertyName(reader);
+
+                if (Options.AttributesAsProperties)
+                {
+
+                    while (lastChar != '>')
+                    {
+                        StringBuilder prop = new StringBuilder(20);
+
+                        StringBuilder val = new StringBuilder(20);
+
+                        c = lastChar;
+
+                        while (char.IsWhiteSpace(c))
+                        {
+                            c = (char)reader.Read();
+                        }
+
+                        do
+                        {
+                            prop.Append(c);
+                            c = (char)reader.Read();
+                        } while (c != '=');
+
+                        reader.AdvancePast('"');
+
+                        c = (char)reader.Read();
+
+                        string pName = prop.ToString();
+
+
+                        if (props.TryGetValue(pName, out PropertyInfo pInfo))
+                        {
+                            do
+                            {
+                                val.Append(c);
+                                c = (char)reader.Read();
+                            } while (c != '"');
+
+                            pInfo.SetValue(o, val.ToString().Convert(pInfo.PropertyType));
+                        }
+                        else
+                        {
+                            do
+                            {
+                                c = (char)reader.Read();
+                            } while (c != '"');
+                        }
+
+                        do
+                        {
+                            c = (char)reader.Read();
+                        } while (char.IsWhiteSpace(c));
+
+                        if (c == '/')
+                        {
+                            reader.AdvancePast('>');
+                            return o;
+                        }
+
+                        lastChar = c;
+                    }
+                }
+
+
+
+                do
+                {
+                    bool selfClosing = false;
+
+                    while (lastChar != '>')
+                    {
+                        lastChar = (char)reader.Read();
+
+                        if (lastChar == '/')
+                        {
+                            selfClosing = true;
+                        }
+                    }
+
+                    if(selfClosing)
+                    {
+                        reader.AdvancePast("<");
+
+                        (propName, lastChar) = GetNextPropertyName(reader);
+
+                        continue;
+                    }
+
+                    if (props.TryGetValue(propName, out PropertyInfo pInfo))
+                    {
+                        pInfo.SetValue(o, GetValue(pInfo.PropertyType, reader));
+                    } else
+                    {
+                        reader.AdvancePast(propName + ">");
+                    }
+
+                    do
+                    {
+                        c = (char)reader.Read();
+                    } while (char.IsWhiteSpace(c));
+
+                    if(c == '/')
+                    {
+                        c = (char)reader.Read();
+                    }
+
+                    (propName, lastChar) = GetNextPropertyName(reader);
+
+
+                } while (propName[0] != '/');
+
+            }
+
+            return o;
+
         }
 
         private void SerializeProperty(PropertyInfo PInfo, object Source, StringBuilder sb)
