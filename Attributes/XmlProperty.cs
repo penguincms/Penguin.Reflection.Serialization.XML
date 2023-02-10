cd0af1290dad297707928874198fd020c8ddd9cd
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 12:12:57 -0700

    Autocommit
    Autocommit

--- Attributes/XmlProperty.cs
+++ Attributes/XmlProperty.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Penguin.Reflection.Serialization.XML.Attributes
 {
     public class XmlPropertyAttribute : Attribute
     {
         public string Name { get; private set; }
+
         public XmlPropertyAttribute(string name)
         {
             this.Name = name;
